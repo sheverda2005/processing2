4017a9604c8444cd978aca0101d6f055a7915cfa
fimal12456 <fimalneol@gmail.com>
2025-10-19 22:54:59 +0300

    fisrt commit

--- src/componens/Login/Login.tsx
+++ src/componens/Login/Login.tsx
@@ -16,7 +16,7 @@ export default function Login() {
                 <h3>Авторизация</h3>
                 <div className={styles.profile_inputs}>
                     <div className={styles.email}>
-                        <label htmlFor="email">Email</label>
+                        <label htmlFor="email">Логин</label>
                         <input onChange={(event)=> {
                             emailInput(event.target.value)
                         }} value={email} className={styles.input_login} type="text" placeholder={"janedoe@gmail.com"}/>
